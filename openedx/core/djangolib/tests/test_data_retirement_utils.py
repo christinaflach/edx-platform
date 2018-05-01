9aaf560fc04a5ff25c2f393a99c0b1a5cd712b31
sandroroux <aroux@edx.org>
2018-05-01 15:51:52 -0400

    Adds tests for deleting Grant.

--- openedx/core/djangolib/tests/test_data_retirement_utils.py
+++ openedx/core/djangolib/tests/test_data_retirement_utils.py
@@ -1,9 +1,10 @@
+import datetime
 from uuid import uuid4
 
 from django.test import TestCase
 from openedx.core.djangoapps.oauth_dispatch.tests import factories
 from student.tests.factories import UserFactory
-from oauth2_provider.models import AccessToken, Application, RefreshToken
+from oauth2_provider.models import AccessToken, Application, RefreshToken, Grant
 
 from ..data_retirement_utils import (
     delete_from_oauth2_provider_accesstoken,
@@ -60,3 +61,23 @@ class TestRetireUserFromOauth2RefreshToken(TestCase):
         delete_from_oauth2_provider_refreshtoken(self.user)
         refresh_tokens = RefreshToken.objects.filter(user_id=self.user.id)
         self.assertFalse(refresh_tokens.exists())
+
+
+class TestRetireUserFromOauth2Grant(TestCase):
+
+    def setUp(self):
+        super(TestRetireUserFromOauth2Grant, self).setUp()
+        self.user = UserFactory.create()
+        self.app = factories.ApplicationFactory(user=self.user)
+
+    def test_delete_from_oauth2_accesstoken(self):
+        Grant.objects.create(
+            user=self.user,
+            application=self.app,
+            expires=datetime.datetime(2018, 1, 1),
+        )
+        delete_from_oauth2_provider_grant(self.user)
+        grants = Grant.objects.filter(
+            user=self.user,
+        )
+        self.assertFalse(grants.exists())
